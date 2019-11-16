b931a0064352f4a07661e44744e7e7077ca71c2b
Ryosuke Araki <ryorsk@mprg.cs.chubu.ac.jp>
2019-11-16 18:52:07 +0900

    update detail of research
    Introduce the modal window, so that can show the research content.

---
+++ assets/js/modalwindow.js
@@ -0,0 +1,18 @@
+(function($) {
+	var winScrollTop;
+	$('.js-modal-open').each(function(){
+		$(this).on('click',function(){
+			// get scroll value
+			winScrollTop = $(window).scrollTop();
+			var target = $(this).data('target');
+			var modal = document.getElementById(target);
+			$(modal).fadeIn();
+			return false;
+		});
+	});
+	$('.js-modal-close').on('click',function(){
+		$('.js-modal').fadeOut();
+		$('body,html').stop().animate({scrollTop:winScrollTop}, 100);
+		return false;
+	}); 
+})(jQuery);
